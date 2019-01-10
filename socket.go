2db5ee437a861868c8a9ee199b2525f53e12b685
hogehoge789 <hogehoge789@gmail.com>
2019-01-10 09:34:33 +0900

    modified:   socket.go

--- socket.go
+++ socket.go
@@ -1,47 +1,67 @@
 package main
-import(
+
+import (
 	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"strings"
+	"time"
 )
 
-func main(){
-	listener, err := net.Listen("tcp","localhost:8080")
-	if err != nil{
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Server is running at localhost:8080")
+	fmt.Println("Server is running at localhost:8888")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			panic(err)
 		}
-		go func(){
-			fmt.Println("Accept %v\n", conn.RemoteAddr())
-			// リクエストを読み込む
-			request, err := http.ReadRequest(
-				bufio.NewReader(conn))
-			if err != nil{
-				panic(err)
+		go func() {
+			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+			// Accept後のソケットで何度も応答を返すためにループ
+			for {
+				// タイムアウトを設定
+				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+				// リクエストを読み込む
+				request, err := http.ReadRequest(bufio.NewReader(conn))
+				if err != nil {
+					// タイムアウトもしくはソケットクローズ時は終了
+					// それ以外はエラーにする
+					neterr, ok := err.(net.Error) // ダウンキャスト
+					if ok && neterr.Timeout() {
+						fmt.Println("Timeout")
+						break
+					} else if err == io.EOF {
+						break
+					}
+					panic(err)
+				}
+				// リクエストを表示
+				dump, err := httputil.DumpRequest(request, true)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println(string(dump))
+				content := "Hello World\n"
+				// レスポンスを書き込む
+				// HTTP/1.1かつ、ContentLengthの設定が必要
+				response := http.Response{
+					StatusCode:    200,
+					ProtoMajor:    1,
+					ProtoMinor:    1,
+					ContentLength: int64(len(content)),
+					Body: ioutil.NopCloser(
+						strings.NewReader(content)),
+				}
+				response.Write(conn)
 			}
-			dump, err := httputil.DumpRequest(request,true)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(string(dump))
-			// レスポンスを書き込む
-			response := http.Response{
-				StatusCode: 200,
-				ProtoMajor: 1,
-				ProtoMinor: 0,
-				Body:       ioutil.NopCloser(
-					strings.NewReader("Hello World\n")),
-		    }
-			response.Write(conn)
 			conn.Close()
 		}()
 	}
